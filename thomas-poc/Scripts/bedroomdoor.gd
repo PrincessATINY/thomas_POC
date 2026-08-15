3983d40307f4db03d18cf396c742069369d6e992
Ziqura Thomas <piercethemcnugget@gmail.com>
2026-08-15 03:17:05 -0500

    Scene Transistions
    Created scripts for scene transistions

---
+++ thomas-poc/Scripts/bedroomdoor.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var player_entered = false
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	player_entered = true
+
+func _on_body_exited(body: CharacterBody2D) -> void:
+	player_entered = false
+	
+func _process(delta):
+	if player_entered == true:
+		if Input.is_action_just_pressed("leftClick"):
+			get_tree().change_scene_to_file("res://Scenes/kitchen.tscn")
+			
--- thomas-poc/Scripts/kitchendoor.gd
+++ thomas-poc/Scripts/kitchendoor.gd
@@ -2,17 +2,13 @@ extends Area2D
 
 var player_entered = false
 
-
-
 func _on_body_entered(body: CharacterBody2D) -> void:
 	player_entered = true
-
-
+	
 func _on_body_exited(body: CharacterBody2D) -> void:
 	player_entered = false
 	
 func _process(delta):
 	if player_entered == true:
 		if Input.is_action_just_pressed("leftClick"):
-			pass
-			#https://www.bing.com/videos/riverview/relatedvideo?q=godot+scene+transition+when+clicking+on+2d+object+&&mid=9A9785AD688214AD7F1B9A9785AD688214AD7F1B&churl=https%3a%2f%2fwww.youtube.com%2fchannel%2fUCSehnx-yuPok8rOWlPIGGOQ&mmscn=mtsc&aps=64&FORM=VMSOVR
+			get_tree().change_scene_to_file("res://Scenes/bedroom.tscn")
