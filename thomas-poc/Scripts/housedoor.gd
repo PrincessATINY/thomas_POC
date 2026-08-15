e2c4a9b38cefe91bc44edd4f6e790105aa055d0f
Ziqura Thomas <piercethemcnugget@gmail.com>
2026-08-15 04:03:56 -0500

    Edits
    Simple edits

---
+++ thomas-poc/Scripts/housedoor.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var player_entered = false
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	player_entered = true
+
+func _on_body_exited(body: CharacterBody2D) -> void:
+	player_entered = false
+	
+func _process(delta):
+	if player_entered == true:
+		if Input.is_action_just_pressed("leftClick"):
+			get_tree().change_scene_to_file("res://Scenes/livingroom.tscn")
